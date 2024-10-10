22c71b18ee74f22b80223f0816e24d594e56ead3
Moh Amilin <amilinmoh99@gmail.com>
2024-10-10 23:19:47 +0700

    add

--- src/middleware.js
+++ src/middleware.js
@@ -1,81 +1,12 @@
-import { NextResponse } from "next/server"
-// import subdomains from "../subdomains.json"
-
-
-const subdomains = [
-  {
-    "subdomain": "client1"
-  },
-  {
-    "subdomain": "amilin"
-  },
-  {
-    "subdomain": "323232"
-  },
-  {
-    "subdomain": "satu"
-  },
-  {
-    "subdomain": "hai"
-  },
-  {
-    "subdomain": "pala"
-  },
-  {
-    "subdomain": "subdomain"
-  },
-  {
-    "subdomain": "see"
-  }
-]
-
-
-
+import { NextResponse } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(request) {
+  return NextResponse.redirect(new URL('/', request.url))
+}
+ 
+// See "Matching Paths" below to learn more
 export const config = {
   // matcher: ["/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)"]
-  matcher: ["/((?!api|_next|static|_vercel|favicon.ico).*)"]
-
-}
-
-export default async function middleware(req) {
-  const url = new URL(req.url)
-  const hostname = req.headers.get("host") || ""
-
-  // Define list of allowed domains
-  // (including localhost and your deployed domain)
-  const allowedDomains = [
-    "localhost:3000",
-    "trillionclues.com.com",
-    "yourdomain.com",
-    "momentify.com",
-    "subdomain.localhost"
-  ]
-
-  // Check if the current hostname is in the list of allowed domains
-  const isAllowedDomain = allowedDomains.some(domain =>
-    hostname.includes(domain)
-  )
-
-  // Extract the potential subdomain from the URL
-  const subdomain = hostname.split(".")[0]
- 
-  // If user is on an allowed domain and it's not a subdomain, allow the request
-  if (isAllowedDomain && !subdomains.some(d => d.subdomain === subdomain)) {
-    return NextResponse.next()
-  } else {
-    console.log({subdomain}, '121')
-
-  }
-
-  const subdomainData = subdomains.find(d => d.subdomain === subdomain)
-
-  console.log({subdomainData})
-  if (subdomainData) {
-    // Rewrite the URL to a dynamic path based on the subdomain
-    return NextResponse.rewrite(
-      new URL(`/${subdomain}${url.pathname}`, req.url)
-    )
-  }
-
-  return new Response(null, { status: 404 })
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 }
